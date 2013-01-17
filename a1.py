9fe0e91ee736138c59204eed5a4e6eef389fe3a5
James Hilliard <james.hilliard1@gmail.com>
2013-01-17 17:53:17 +0000

    Initial

---
+++ a1.py
@@ -0,0 +1,20 @@
+def Count(x,s):
+    return len([1 for e in s if e == x])
+
+def Percentages(alphabet,string):
+    return [e,round((count/len(string))*100)) for alphabet in string 
+    #l = []
+    #for e in alphabet:
+        #count = 0
+        #for f in string:
+            #if e == f:
+                #count += 1
+        #l += [(e,round((count/len(string))*100))]
+    #return l
+
+def RandomString(lenght,distribution):
+    s = []
+    for e in distribution:
+        
+
+    for e in range(len(10))
